ea18c4c664a30b005100aaa2c8170cc03059f2aa
MSE <polmse@yahoo.com>
2020-06-07 19:41:58 +0200

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,45 @@
+let age = 30;
+console.log(age);
+let ww2 = 6;
+console.log(ww2);
+let tesla = 76*885.66;
+console.log(tesla);
+let secondsOfDay = 86400;
+console.log(secondsOfDay);
+let hungarians = 9661855/7789771263;
+console.log(hungarians);
+let bigger = 1*122;
+console.log(bigger);
+let participants = 654-7;
+console.log(participants);
+let names = ['Trevor','Ginny','Mark'];
+console.log(names);
+console.log(names[2].length); 
+if(names[2].length < 5) {
+    console.log("false");
+      }
+names.push('zoltan');
+console.log(names);
+names[0] = 'Emese';
+console.log(names);
+names.push('Karl');
+if(names.length > 4) {
+    console.log("true");
+      }
+console.log(names);
+
+let something = {
+    colors: ["blue","white","black","purple","pink","red"],
+    hasManyColors: ["green","brown","yellow","grey"]
+  };
+console.log(something.hasManyColors);  
+console.log(something.hasManyColors.length);
+if(something.hasManyColors.length > 3) {
+    console.log("true");
+      }
+
+
+
+
+
+
